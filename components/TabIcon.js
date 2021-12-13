c7a36f5b710fb78f6e749b669ff67e007942d35d
zeealik <zeealik7@gmail.com>
2021-12-13 12:50:13 +0500

    tabicon added file

---
+++ components/TabIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const TabIcon = () => {
+   return (
+      <View>
+         <Text></Text>
+      </View>
+   )
+}
+
+export default TabIcon
+
+const styles = StyleSheet.create({})
